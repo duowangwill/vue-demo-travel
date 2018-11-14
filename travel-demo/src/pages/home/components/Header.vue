74974360c1c77abdcfa41c7e011d5a7fe1f8284f
skybluerun <skybluerun>
2018-11-14 19:03:49 +0800

    project init

--- travel-demo/src/pages/home/components/Header.vue
+++ travel-demo/src/pages/home/components/Header.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="header">
-        <div class="header-left"></div>
-        <div class="input"></div>
+        <div class="header-left">
+            <div class="iconfont back-icon"></div>
+        </div>
+        <div class="input">
+            <span class="iconfont"></span>
+            输入城市/景点/游玩主题
+        </div>
         <div class="right"></div>
     </div>
 </template>
