22d5bf5da6715dba3392516b99cd22a8f71435d4
skybluerun <skybluerun>
2018-11-14 16:22:06 +0800

    some code

---
+++ travel-demo/src/pages/home/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="header">
+        <div class="header-left"></div>
+        <div class="input"></div>
+        <div class="right"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeHeader'
+}
+</script>
+
