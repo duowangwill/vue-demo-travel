885d83bb6e96f7809f854c1c8f8d08785d80c1d1
skybluerun <skybluerun>
2018-11-15 18:06:12 +0800

    add styles files

--- travel-demo/src/App.vue
+++ travel-demo/src/App.vue
@@ -4,5 +4,10 @@
   </div>
 </template>
 
-<style>
-</style>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style></style>
--- travel-demo/src/main.js
+++ travel-demo/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'babel-polyfill'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
 
